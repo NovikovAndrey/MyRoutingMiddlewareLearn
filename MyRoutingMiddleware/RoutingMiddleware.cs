7e2601d3fffd84f6f541246a87d6b333c7d3f9e0
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-14 10:13:22 +0300

    Add project files.

---
+++ MyRoutingMiddleware/RoutingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRoutingMiddleware
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+        public RoutingMiddleware(RequestDelegate request)
+        {
+            requestDelegate = request;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path=="/index")
+            {
+                await context.Response.WriteAsync("Home page");
+            }
+        }
+
+    }
+}
