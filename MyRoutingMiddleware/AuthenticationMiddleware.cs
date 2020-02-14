e470db9b12a3727dc9ec9683bb75082473e700c5
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-14 10:22:23 +0300

    Add AuthenticationMiddleware

---
+++ MyRoutingMiddleware/AuthenticationMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRoutingMiddleware
+{
+    public class AuthenticationMiddleware
+    {
+        private RequestDelegate requestDelegate;
+        public AuthenticationMiddleware(RequestDelegate request)
+        {
+            requestDelegate = request;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Access denied");
+            }
+            else
+            {
+                await requestDelegate.Invoke(context);
+            }
+        }
+    }
+}
